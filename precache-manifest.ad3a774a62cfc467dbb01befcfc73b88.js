656cffa6213b97f50db406a7ee0cd6b3125f916e
Jorge Arturo Suárez Uribe <36580792+frontendjorge@users.noreply.github.com>
2019-12-15 20:43:27 -0500

    first release

---
+++ precache-manifest.ad3a774a62cfc467dbb01befcfc73b88.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bcf5e0b652ddacd5c2ae5c97c45cce",
+    "url": "/zemoga/index.html"
+  },
+  {
+    "revision": "8e6cf947a17470eb6ec4",
+    "url": "/zemoga/static/css/main.92773fc3.chunk.css"
+  },
+  {
+    "revision": "5b114b01b5332d80820b",
+    "url": "/zemoga/static/js/2.5757e49d.chunk.js"
+  },
+  {
+    "revision": "8e6cf947a17470eb6ec4",
+    "url": "/zemoga/static/js/main.54939699.chunk.js"
+  },
+  {
+    "revision": "a3d3681cb9401f79a4fa",
+    "url": "/zemoga/static/js/runtime~main.2430f44c.js"
+  },
+  {
+    "revision": "12a234e87f37d441adc9bb2e3dec13de",
+    "url": "/zemoga/static/media/Lato-Bold.12a234e8.woff2"
+  },
+  {
+    "revision": "2032f2b161a7a109a9d0cc3f6ad3b735",
+    "url": "/zemoga/static/media/Lato-Bold.2032f2b1.woff"
+  },
+  {
+    "revision": "09201a1cc16b4ea1d1b7c8fdf90d5a60",
+    "url": "/zemoga/static/media/Lato-Light.09201a1c.woff2"
+  },
+  {
+    "revision": "4ac54dd4f4c6e2a86e21aee167e1a5a1",
+    "url": "/zemoga/static/media/Lato-Light.4ac54dd4.woff"
+  },
+  {
+    "revision": "69279aef7fbc11101022a9f06079bbbf",
+    "url": "/zemoga/static/media/Lato-Regular.69279aef.woff2"
+  },
+  {
+    "revision": "e87e43c57f21dd637791c6bfa534c1af",
+    "url": "/zemoga/static/media/Lato-Regular.e87e43c5.woff"
+  },
+  {
+    "revision": "2901d17ea51abbed40dd440767eae409",
+    "url": "/zemoga/static/media/bg-banners.2901d17e.png"
+  },
+  {
+    "revision": "88160ab40c926b94bf64f78f9f9da761",
+    "url": "/zemoga/static/media/calltoaction-footer.88160ab4.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/zemoga/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/zemoga/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/zemoga/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/zemoga/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/zemoga/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
